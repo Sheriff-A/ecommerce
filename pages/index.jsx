b1a3842833a78548491f665768a3a34ee8384b33
Sheriff-A <sheriff@experiencedriven.ca>
2022-06-15 18:30:30 -0400

    retreive and pass data from sanity

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,23 +1,40 @@
 import React from "react";
 
-import { Product, HeroBanner, FooterBanner } from "../components"
+import { client } from "../lib/client";
+import { Product, HeroBanner, FooterBanner } from "../components";
 
-const Home = () => {
+const Home = ({ products, bannerData }) => {
   return (
     <>
-      <HeroBanner />
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
+      {console.log(bannerData)}
       <div className="products-heading">
         <h2>Best Selling Product</h2>
         <p>Speakers of Many Variations</p>
       </div>
       <div className="products-container">
-        {["Product 1", "Product 2"].map((product) => {
-          return product;
+        {products?.map((product) => {
+          return <Product key={product._id} product={product} />;
         })}
       </div>
-      <FooterBanner />
+      <FooterBanner footerBanner={bannerData && bannerData[0]} />
     </>
   );
+};
+
+export const getServerSideProps = async () => {
+  const productQuery = '*[_type == "product"]'; // Grab all products from sanity
+  const products = await client.fetch(productQuery);
+
+  const bannerQuery = '*[_type == "banner"]'; // Grab all banners from sanity
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: {
+      products,
+      bannerData,
+    },
+  };
 };
 
 export default Home;
